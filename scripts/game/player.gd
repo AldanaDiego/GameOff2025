461c27bd1d22a4436b9d776f1f3650eb8109a705
Toson D. <daldana9@gmail.com>
2025-11-11 13:00:03 -0300

    Vehicle animations, water tank shader

--- scripts/game/player.gd
+++ scripts/game/player.gd
@@ -5,12 +5,16 @@ class_name Player extends VehicleBody3D
 
 const STEER_SPEED: float = 1.5
 const MAX_STEER: float = 0.4
-const ENGINE_POWER: float = 40.0
+const ENGINE_POWER: float = 50.0
 const BRAKE_STRENGTH: float = 2.0
-const MAX_SPEED_FOR_ACCEL: float = 5.0
+const MAX_SPEED_FOR_ACCEL: float = 6.0
 const MAX_ENGINE_ACCEL: float = 100.0
 
 @onready var camera_controller: Node3D = $CameraController
+@onready var _animation: AnimationPlayer = $scorpion/AnimationPlayer
+
+func _ready() -> void:
+    _animation.playback_default_blend_time = 1
 
 func _physics_process(delta):
     var steer_input: float = Input.get_axis("move_right", "move_left")
@@ -23,11 +27,16 @@ func _physics_process(delta):
     var speed: float = linear_velocity.length()
 
     # I copied this code from Godot Demo Projects I don't really understand it u.u
+    # https://github.com/godotengine/godot-demo-projects/tree/master/3d/truck_town
     if !is_zero_approx(accel_input):
         if speed < MAX_SPEED_FOR_ACCEL and !is_zero_approx(speed):
             engine_force = clampf(ENGINE_POWER * MAX_SPEED_FOR_ACCEL * (BRAKE_STRENGTH if accel_input < 0 else 1.0) / speed, 0.0, MAX_ENGINE_ACCEL) * (-BRAKE_STRENGTH if accel_input < 0 else 1.0)
         else:
-            engine_force = ENGINE_POWER
+            engine_force = ENGINE_POWER * (-BRAKE_STRENGTH if accel_input < 0 else 1.0)
     else:
         engine_force = 0.0
 
+    if engine_force == 0:
+        _animation.play("Idle")
+    else:
+        _animation.play("Drive_Backward" if engine_force < 0 else "Drive_Forward")
